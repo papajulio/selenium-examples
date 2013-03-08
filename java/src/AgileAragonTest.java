7640df7fe2d48179dfcf107de2faf6d8abb9df5d
Julio García <jjgarcia@zentyal.com>
2013-03-08 21:35:48 +0100

    Adding java example with eclipse project files

---
+++ java/src/AgileAragonTest.java
@@ -0,0 +1,58 @@
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class AgileAragonTest {
+
+	static WebDriver driver;
+	static org.openqa.selenium.support.ui.Wait<WebDriver> wait;	
+	
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		wait = new WebDriverWait(driver, 30);
+		driver.get("http://agile-aragon.org/2013/02/25/testing-hacklab-en-marzo/");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.close();
+	}
+
+	@Test
+	public void testImageDontAppearInPost() {
+		Assert.assertFalse(isElementPresent("attachment_65"));
+	}
+	
+	@Test
+	public void testAgileAragonSearchWorks() {
+		
+		WebElement element = driver.findElement(By.id("s"));
+		element.click();
+		element.sendKeys("kata");
+		
+		element = driver.findElement(By.id("searchsubmit"));
+		element.click();
+		
+		Assert.assertTrue(isElementPresent("attachment_65"));
+	}
+	
+	
+	private boolean isElementPresent(String id) {
+		try {
+			driver.findElement(By.id(id));
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
+}
